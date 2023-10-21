3f9eb07cb8cf8575d880d31ef6365731659af576
root <m-zhitkov@inbox.ru>
2023-10-21 13:37:05 +0300

    Created database connection

---
+++ database/database.py
@@ -0,0 +1,65 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class DbSettings(BaseModel):
+    url: str = f"postgresql+asyncpg://admin:admin@db/twitter_clone_db"
+    # echo: bool = False
+    echo: bool = True
+
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+
+    db: DbSettings = DbSettings()
+
+    # db_echo: bool = True
+
+
+settings = Settings()
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
+
+
+db_helper = DatabaseHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
