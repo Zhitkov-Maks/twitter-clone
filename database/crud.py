f56cdca33ebad798bf36d72e46aaa2c6688ac841
root <m-zhitkov@inbox.ru>
2023-10-28 19:07:36 +0300

    Fixed func add_like_in_db

--- database/crud.py
+++ database/crud.py
@@ -5,10 +5,10 @@ from fastapi import HTTPException
 from sqlalchemy import select, func, desc
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload, selectinload, contains_eager
+from sqlalchemy.orm import joinedload
 from starlette import status
 
-from database.models import User, Tweet, Image, likes_table, followers
+from database.models import User, Tweet, Image, likes_table
 from schemas import AddTweetSchema
 
 
@@ -20,18 +20,16 @@ async def add_tweet_in_db(session: AsyncSession, user: User, tweet_in: AddTweetS
     return tweet.tweet_id
 
 
-async def get_all_tweet_followed(session: AsyncSession, user_id: int):
+async def get_all_tweet_followed(session: AsyncSession):
     stmt = (select(Tweet, func.count(likes_table.c.user_id).label("likes"))
-            .join(followers, (followers.c.followed_id == Tweet.user_id))
-            .join(Tweet.likes)
-            .where(followers.c.follower_id == user_id)
+            .join(Tweet.likes, isouter=True)
             .group_by(Tweet.tweet_id)
-            .order_by(desc("likes")).limit(10))
+            .order_by(desc("likes")).limit(25))
     all_tweet = await session.scalars(stmt)
     return all_tweet.unique().all()
 
 
-async def get_tweet_by_id(session: AsyncSession, tweet_id: int) -> Tweet | None:
+async def get_tweet_by_id(session: AsyncSession, tweet_id: int) -> Tweet:
     tweet: Tweet | None = await session.get(Tweet, tweet_id)
     if tweet is not None:
         return tweet
@@ -62,7 +60,7 @@ async def get_user_data_followed(session: AsyncSession, user_id) -> User:
     return user
 
 
-async def get_user_by_api_key(session: AsyncSession, api_key: str) -> User | None:
+async def get_user_by_api_key(session: AsyncSession, api_key: str) -> User:
     stmt = select(User).where(User.api_key == api_key)
     user: User | None = await session.scalar(stmt)
     if user is not None:
@@ -77,7 +75,7 @@ async def get_user_by_api_key(session: AsyncSession, api_key: str) -> User | Non
     )
 
 
-async def get_user_by_id(session: AsyncSession, user_id: int) -> User | None:
+async def get_user_by_id(session: AsyncSession, user_id: int) -> User:
     user: User | None = await session.get(User, user_id)
     if user is not None:
         return user
@@ -103,57 +101,70 @@ async def add_image_in_db(session: AsyncSession, url: str) -> int:
     return img.id
 
 
-async def check_likes(session: AsyncSession, tweet_id: int, user_id: int) -> bool:
-    stmt = select(likes_table).where(
-        (likes_table.c.tweet_id == tweet_id) & (likes_table.c.user_id == user_id))
-    return await session.scalar(stmt)
-
-
-async def add_like_in_db(session: AsyncSession, tweet: Tweet, user_id: int) -> bool | None:
-    exists = await check_likes(session, tweet.tweet_id, user_id)
-    if exists:
-        return False
-
-    user = await get_full_user_data(session, user_id)
-    user.likes.append(tweet)
-    session.add_all(user.likes)
-    await session.commit()
+async def add_like_in_db(session: AsyncSession, tweet: Tweet, user_id: int):
+    try:
+        user = await get_full_user_data(session, user_id)
+        user.likes.append(tweet)
+        session.add_all(user.likes)
+        await session.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "result": False,
+                "error_type": "Bad Request",
+                "error_message": "You've already liked it"
+            }
+        )
 
 
 async def delete_like_in_db(session: AsyncSession, tweet: Tweet, user: User):
-    exists = await check_likes(session, tweet.tweet_id, user.id)
-    if not exists:
-        return False
-    user = await get_full_user_data(session, user.id)
-    user.likes.remove(tweet)
-    await session.commit()
-
-
-async def check_followed(session: AsyncSession, user_id: int, user_followed_id: int) -> bool:
-    stmt = select(followers).where(
-        (followers.c.follower_id == user_id) & (followers.c.followed_id == user_followed_id))
-    return await session.scalar(stmt)
+    try:
+        user = await get_full_user_data(session, user.id)
+        user.likes.remove(tweet)
+        await session.commit()
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "result": False,
+                "error_type": "Bad Request",
+                "error_message": "You've already deleted it"
+            }
+        )
 
 
 async def add_followed(session: AsyncSession, user_id, user_followed):
-    check_follow = await check_followed(session, user_id, user_followed.id)
-    if check_follow:
-        return False
-
-    user = await get_user_data_followed(session, user_id)
-    user.followed.append(user_followed)
-    session.add_all(user.followed)
-    await session.commit()
+    try:
+        user = await get_user_data_followed(session, user_id)
+        user.followed.append(user_followed)
+        session.add_all(user.followed)
+        await session.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "result": False,
+                "error_type": "Bad Request",
+                "error_message": f"User has already been added"
+            }
+        )
 
 
 async def remove_followed(session: AsyncSession, user_id, user_followed):
-    check_follow = await check_followed(session, user_id, user_followed.id)
-    if not check_follow:
-        return False
-
-    user = await get_user_data_followed(session, user_id)
-    user.followed.remove(user_followed)
-    await session.commit()
+    try:
+        user = await get_user_data_followed(session, user_id)
+        user.followed.remove(user_followed)
+        await session.commit()
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "result": False,
+                "error_type": "Bad Request",
+                "error_message": f"User has already been deleted"
+            }
+        )
 
 
 async def get_image_url(session: AsyncSession, image_id_list: List[int]):
