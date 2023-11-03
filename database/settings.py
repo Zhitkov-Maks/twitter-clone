68b6853e56aa0c6628a6003840e6cb649f3bc2cf
root <m-zhitkov@inbox.ru>
2023-11-03 21:18:30 +0300

    Added environment variables

--- database/settings.py
+++ database/settings.py
@@ -4,21 +4,18 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.pool import NullPool
 
+from database.config import DB_USER, DB_PASS, DB_NAME
 
-DATABASE_URL: str = f"postgresql+asyncpg://admin:admin@db/twitter_clone_db"
+DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@db/{DB_NAME}"
 Base = declarative_base()
 
 
 engine = create_async_engine(DATABASE_URL, echo=True, poolclass=NullPool)
 async_session_maker = async_sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=False
+    bind=engine, autoflush=False, autocommit=False, expire_on_commit=False
 )
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-
     async with async_session_maker() as session:
         yield session
