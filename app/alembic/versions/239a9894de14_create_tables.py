3331f2f9f4b4cce03e08a1692e65b76ecc4bddec
maksim <m-zhitkov@inbox.ru>
2023-11-11 17:42:06 +0300

    Fixed linter

--- app/alembic/versions/239a9894de14_create_tables.py
+++ app/alembic/versions/239a9894de14_create_tables.py
@@ -7,9 +7,8 @@ Create Date: 2023-11-05 23:10:00.994831
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "239a9894de14"
