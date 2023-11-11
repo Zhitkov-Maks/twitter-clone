3c0997e7541e4e5e9fb9c8018522e7e0b69eb4cc
maksim <m-zhitkov@inbox.ru>
2023-11-11 17:38:47 +0300

    Linter fix

--- app/main.py
+++ app/main.py
@@ -6,17 +6,16 @@ also contains an endpoint for loading images.
 """
 from typing import Dict
 
-from fastapi import Depends, FastAPI, File, HTTPException, Security, UploadFile
-from fastapi.security import APIKeyHeader
-from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
-
 from crud.user import get_user_by_api_key
+from fastapi import Depends, FastAPI, File, Security, UploadFile
+from fastapi.security import APIKeyHeader
 from models.db_conf import get_async_session
 from routes.tweet_route import route_tw
 from routes.user_route import route_us
 from schemas.tweet_schema import ReturnImageSchema
 from service import read_and_write_image
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 app = FastAPI()
 app.include_router(route_us)
