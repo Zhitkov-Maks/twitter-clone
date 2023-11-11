daa6087f0e4e72846121ea5e7b793c16666ae4b6
maksim <m-zhitkov@inbox.ru>
2023-11-11 23:12:14 +0300

    Added docstring test_user.py

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -2,6 +2,7 @@ from httpx import AsyncClient
 
 
 async def test_get_user_me(ac: AsyncClient):
+    """Test for obtaining information about a user with valid data."""
     response = await ac.get("/api/users/me", headers={"api-key": "test"})
     data = response.json()
     assert data.get("result"), data.get("user")
@@ -9,6 +10,8 @@ async def test_get_user_me(ac: AsyncClient):
 
 
 async def test_get_tweets_user_is_not_register(ac: AsyncClient):
+    """Test for obtaining information about the
+    user if the user is not in the database."""
     response = await ac.get("/api/users/me", headers={"api-key": "notRegisterUser"})
     data = response.json()
     assert not data.get("result")
@@ -16,6 +19,7 @@ async def test_get_tweets_user_is_not_register(ac: AsyncClient):
 
 
 async def test_get_user_by_id(ac: AsyncClient):
+    """Test for obtaining information about the user by ID."""
     response = await ac.get("/api/users/1", headers={"api-key": "test"})
     data = response.json()
     assert data.get("result"), data.get("user")
@@ -23,6 +27,8 @@ async def test_get_user_by_id(ac: AsyncClient):
 
 
 async def test_get_user_by_id_not_exists(ac: AsyncClient):
+    """Test for obtaining information about a user by ID,
+    if the ID does not exist or has been deleted."""
     response = await ac.get("/api/users/100", headers={"api-key": "test"})
     data = response.json()
     assert not data.get("result"), not data.get("user")
@@ -30,6 +36,7 @@ async def test_get_user_by_id_not_exists(ac: AsyncClient):
 
 
 async def test_user_add_followed(ac: AsyncClient):
+    """Test for adding a subscription to another user."""
     response = await ac.post("/api/users/4/follow", headers={"api-key": "test"})
     data = response.json()
     assert data.get("result")
@@ -37,6 +44,7 @@ async def test_user_add_followed(ac: AsyncClient):
 
 
 async def test_user_add_followed_twice(ac: AsyncClient):
+    """Test for the inability to subscribe twice, and the service does not crash."""
     response = await ac.post("/api/users/4/follow", headers={"api-key": "test"})
     data = response.json()
     assert not data.get("result")
@@ -45,6 +53,7 @@ async def test_user_add_followed_twice(ac: AsyncClient):
 
 
 async def test_user_remove_followed(ac: AsyncClient):
+    """Test for removing a user's subscription."""
     response = await ac.delete("/api/users/4/follow", headers={"api-key": "test"})
     data = response.json()
     assert data.get("result")
@@ -52,6 +61,7 @@ async def test_user_remove_followed(ac: AsyncClient):
 
 
 async def test_user_remove_followed_twice(ac: AsyncClient):
+    """Test for removing a user's subscription twice and the service does not crash."""
     response = await ac.delete("/api/users/4/follow", headers={"api-key": "test"})
     data = response.json()
     assert not data.get("result")
@@ -60,6 +70,7 @@ async def test_user_remove_followed_twice(ac: AsyncClient):
 
 
 async def test_user_add_followed_not_exists_user(ac: AsyncClient):
+    """Test for trying to subscribe to a user who is not in the database."""
     response = await ac.post("/api/users/100/follow", headers={"api-key": "test"})
     data = response.json()
     assert data.get("detail").get("error_type") == "Not Found"
