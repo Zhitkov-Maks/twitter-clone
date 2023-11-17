274c1766b53c1604ad89c5861db53de220b6d477
maksim <m-zhitkov@inbox.ru>
2023-11-17 23:38:54 +0300

    Final commit

--- app/config.py
+++ app/config.py
@@ -10,5 +10,15 @@ DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
+# Выбор какие сообщения показывать. От подписчиков или все.
 tweet_followers: bool = False
-number_of_tweets = 25
+
+# Количество твитов в response.
+number_of_tweets: int = 25
+
+# Минимальная и максимальная длина твитов.
+max_length_tweet: int = 10_000
+min_length_tweet: int = 5
+
+# Допустимые форматы картинок.
+allowed_types: tuple = ("image/jpg", "image/png", "image/jpeg")
