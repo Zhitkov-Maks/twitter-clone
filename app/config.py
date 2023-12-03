08512ed6d5e9d6c6e469ab08c8dd9ea620caf5b0
maksim <m-zhitkov@inbox.ru>
2023-12-03 18:52:31 +0300

    Removed extra lines

--- app/config.py
+++ app/config.py
@@ -11,10 +11,10 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 # Выбор какие сообщения показывать. От подписчиков или все.
-tweet_followers: bool = False
+tweet_followers: bool = True
 
 # Количество твитов в response.
-number_of_tweets: int = 25
+number_of_tweets: int = 100
 
 # Минимальная и максимальная длина твитов.
 max_length_tweet: int = 10_000
